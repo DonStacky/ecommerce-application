92a19d0af4ce595a021489c1f971e091c37e21f2
Сергей (DonStacky) <rodnihsergey1994@mail.ru>
2023-08-31 14:27:49 +0300

    feat: add test card

---
+++ src/pages/catalog/catalog-data.ts
@@ -0,0 +1,35 @@
+import buildCommonClient from '../../shared/api/create-common-client';
+import { createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
+import checkEnvVariables from '../../shared/helpers/utilites';
+
+async function getProducts() {
+  const ctpClient = buildCommonClient();
+  const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
+    projectKey: checkEnvVariables(process.env.CTP_PROJECT_KEY),
+  });
+
+  return apiRoot.products().get().execute();
+}
+
+export const productsImgURL = getProducts()
+  .then(({ body }) => {
+    let img: string | undefined;
+    const length = body.results[0].masterData.current.masterVariant.images?.length;
+    if (length) {
+      img = body.results[0].masterData.current.masterVariant.images?.[length - 1].url;
+    }
+    return img;
+  })
+  .catch(console.error);
+
+export const productsTitle = getProducts()
+  .then(({ body }) => {
+    return body.results[0].masterData.current.name.en;
+  })
+  .catch(console.error);
+
+export const productsText = getProducts()
+  .then(({ body }) => {
+    return body.results[0].masterData.current.description?.en;
+  })
+  .catch(console.error);
--- src/pages/catalog/catalog.ts
+++ src/pages/catalog/catalog.ts
@@ -1,14 +1,83 @@
 import { createElement } from '../../shared/helpers/dom-utilites';
+// import sailboat from '@image/sailboat.jpg';
+import './catalog.scss';
+import { productsImgURL, productsTitle, productsText } from './catalog-data';
 
-const CATALOG_TITLE = createElement({
-  tagname: 'h1',
-  options: [['textContent', 'There will be a "Catalog" page']],
+console.log(productsImgURL);
+
+const cardTitle = createElement({
+  tagname: 'h5',
+  options: [['className', 'card-title']],
+});
+productsTitle.then((title) => {
+  cardTitle.textContent = `${title}`;
+});
+
+const cardText = createElement({
+  tagname: 'p',
+  options: [
+    ['textContent', `Some quick example text to build on the card title and make up the bulk of the card's content.`],
+    ['className', 'card-text'],
+  ],
+});
+productsText.then((text) => {
+  cardText.textContent = `${text}`;
+});
+
+const cardBtn = createElement({
+  tagname: 'a',
+  options: [
+    ['textContent', `Go somewhere`],
+    ['className', 'btn btn-primary'],
+    ['href', '#'],
+  ],
+});
+
+const cardMore = createElement({
+  tagname: 'a',
+  options: [
+    ['textContent', `More...`],
+    ['className', 'card-link'],
+  ],
+});
+function toggleCardDescription() {
+  cardText.classList.toggle('card-text--active');
+  if (cardMore.textContent === 'Less...') {
+    cardMore.textContent = 'More...';
+  } else {
+    cardMore.textContent = 'Less...';
+  }
+}
+cardMore.addEventListener('click', toggleCardDescription);
+
+const cardInner = createElement({
+  tagname: 'div',
+  options: [['className', 'card-body']],
+  childElements: [cardTitle, cardText, cardMore, cardBtn],
+});
+
+const cardImg = createElement({
+  tagname: 'img',
+  options: [
+    ['className', 'card-img-top'],
+    ['alt', 'card-img-top'],
+  ],
+});
+productsImgURL.then((url) => cardImg.setAttribute('src', `${url}`));
+
+const card = createElement({
+  tagname: 'div',
+  options: [
+    ['className', 'card'],
+    ['style', 'width: 18rem;'],
+  ],
+  childElements: [cardImg, cardInner],
 });
 
 const CATALOG_PAGE = createElement({
   tagname: 'section',
   options: [['className', 'catalog d-flex justify-content-center align-items-center']],
-  childElements: [CATALOG_TITLE],
+  childElements: [card],
 });
 
 export default CATALOG_PAGE;
