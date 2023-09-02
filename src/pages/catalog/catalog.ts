68ee8f9584f6f0f976ed6876b79c89f9569ee702
user42022 <andrekizilov@yandex.ru>
2023-09-03 01:21:36 +0300

    feat: create search form from html template

--- src/pages/catalog/catalog.ts
+++ src/pages/catalog/catalog.ts
@@ -1,14 +1,20 @@
 import { createElement } from '../../shared/helpers/dom-utilites';
+// import { ACCORDION } from './side-bar';
+import { SEARCH_BAR } from './search-form';
+import 'bootstrap';
 
-const CATALOG_TITLE = createElement({
-  tagname: 'h1',
-  options: [['textContent', 'There will be a "Catalog" page']],
-});
+// const CATALOG_TITLE = createElement({
+//   tagname: 'h1',
+//   options: [['textContent', 'There will be a "Catalog" page']],
+// });
 
 const CATALOG_PAGE = createElement({
   tagname: 'section',
-  options: [['className', 'catalog d-flex justify-content-center align-items-center']],
-  childElements: [CATALOG_TITLE],
+  options: [['className', 'catalog']],
+  childElements: [
+    /* CATALOG_TITLE */
+    SEARCH_BAR,
+  ],
 });
 
 export default CATALOG_PAGE;
